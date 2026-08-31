4373c4056370d1368347fba44d0253917dc80a15
Divya-codes2 <divyadhiman012@gmail.com>
2026-08-31 23:27:55 +0530

    Added reverse array program

---
+++ reverse_array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[9] = {1,2,3,4,5,6,7,8,9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int i = 0;
+    int j = n - 1;
+
+    while (i < j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        i++;
+        j--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
